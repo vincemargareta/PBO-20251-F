5a2fcd69592f6881808c8a416398852320f40505
vince margareta sihotang <vincemargaretasihotang0@gmail.com>
2025-11-09 13:26:55 +0700

    first commit

---
+++ LuasLingkaran.php
@@ -0,0 +1,13 @@
+<?php
+
+class LuasLingkaran {
+    public const phi = 3.14;
+    public int $jari;
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 5;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+echo "Hasilnya adalah: ". $rumus;
+
